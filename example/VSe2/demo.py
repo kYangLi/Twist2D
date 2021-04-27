816d3fef358288b72d1d47b889eb0e63dff71eef
yangli <lyang.1915@gmail.com>
2021-04-27 20:24:54 +0800

    version 0.3.1, simplify the module loading

--- example/VSe2/demo.py
+++ example/VSe2/demo.py
@@ -8,9 +8,6 @@ from twist2d import *
 # Create an object for t2d
 twist_demo = Twist2D()
 
-# Read in the primitive cell information of each layer
-twist_demo.read_primcell_of_layers('./POSCAR', 3)
-
 # Initialize the different twisted layers 
 # (for different primitive cell condition)
 ### For the primitive cell of VSe2 in 120 degree
@@ -20,11 +17,11 @@ n = 8
 # --> 1st layer
 mult_a1p = [m, n]
 mult_a2p = [-n, m-n]
-twist_demo.add_layer(mult_a1p, mult_a2p, layer_dis=3)
+twist_demo.add_layer(mult_a1p, mult_a2p, layer_dis=3, prim_poscar="POSCAR")
 # --> 2nd layer
 mult_a1p = [n, m]
 mult_a2p = [-m, n-m]
-twist_demo.add_layer(mult_a1p, mult_a2p)
+twist_demo.add_layer(mult_a1p, mult_a2p, prim_poscar="POSCAR")
 
 # ### For the primitive cell of VSe2 in 60 degree (more common)
 # m = 7
@@ -32,11 +29,11 @@ twist_demo.add_layer(mult_a1p, mult_a2p)
 # # --> 1st layer
 # mult_a1p = [m, n]
 # mult_a2p = [-n, m+n]
-# twist_demo.add_layer(mult_a1p, mult_a2p, layer_dis=3)
+# twist_demo.add_layer(mult_a1p, mult_a2p, layer_dis=3, prim_poscar="POSCAR")
 # # --> 2nd layer
 # mult_a1p = [n, m]
 # mult_a2p = [-m, n+m]
-# twist_demo.add_layer(mult_a1p, mult_a2p)
+# twist_demo.add_layer(mult_a1p, mult_a2p, prim_poscar="POSCAR")
 
 # Fill the cell with the layers
 twist_demo.twist_layers(start_z=0.1)
